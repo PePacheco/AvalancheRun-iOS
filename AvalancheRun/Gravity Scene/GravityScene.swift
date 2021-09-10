cbdd820928ebbd7ff611967bd4ac24059197f6ad
Santiago <santiago.dgonzaga@hotmail.com>
2021-09-10 16:28:27 -0300

    creating avalanche

--- AvalancheRun/Gravity Scene/GravityScene.swift
+++ AvalancheRun/Gravity Scene/GravityScene.swift
@@ -2,16 +2,17 @@
 //  GameScene.swift
 //  AvalancheRun
 //
-//  Created by Pedro Gomes Rubbo Pacheco on 09/09/21.
+//  Created by Santiago del Castillo Gonzaga on 09/09/21.
 //
 
 import SpriteKit
 import GameplayKit
 import CoreMotion
 
-class GravityScene: SKScene {
+class GravityScene: SKScene, SKPhysicsContactDelegate {
     
     private var player = SKShapeNode(circleOfRadius: 15)
+    private var avalanche = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width, height: 300))
     private var cameraNode = SKCameraNode()
     private var motionManager = CMMotionManager()
     private var pointsLabel = SKLabelNode()
@@ -26,6 +27,8 @@ class GravityScene: SKScene {
         configureBallMovement()
         addCamera()
         createLabel()
+        createAvalanche()
+        physicsWorld.contactDelegate = self
         
         for _ in 0...numberOfFutureGrounds {
             createGround()
@@ -33,28 +36,45 @@ class GravityScene: SKScene {
     }
     
     func createPlayer() {
-        player.position = CGPoint(x: 0, y: 0)
+        player.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
         player.strokeColor = SKColor.black
         player.glowWidth = 1.0
         player.fillColor = SKColor.orange
         player.physicsBody = SKPhysicsBody(circleOfRadius: 15)
         player.physicsBody?.affectedByGravity = true
         player.physicsBody?.isDynamic = true
+        player.physicsBody?.categoryBitMask = 0b001
+        player.physicsBody?.collisionBitMask = 0b110
+        player.name = "player"
         
         addChild(player)
+    }
+    
+    func createAvalanche() {
+        avalanche.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: 200)
+        avalanche.fillColor = SKColor.white
+        avalanche.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: UIScreen.main.bounds.width, height: 300))
+        avalanche.physicsBody?.applyForce(CGVector(dx: 0, dy: -20))
+        avalanche.physicsBody?.categoryBitMask = 0b010
+        avalanche.physicsBody?.collisionBitMask = 0b001
+        avalanche.name = "avalanche"
+        
+        addChild(avalanche)
     }
     
     func createLabel() {
         pointsLabel.fontSize = 32
         pointsLabel.fontColor = UIColor.systemPink
-        pointsLabel.position.x = 0
+        pointsLabel.position.x = UIScreen.main.bounds.width / 2
         pointsLabel.position.y = 100
         pointsLabel.text = String(points)
+        
+        addChild(pointsLabel)
     }
     
     func updateLabel() {
+        points += 1
         pointsLabel.text = String(points)
-        pointsLabel.position.y = CGFloat(100 + grounds * groundHeight)
     }
     
     func addCamera() {
@@ -73,6 +93,8 @@ class GravityScene: SKScene {
         
         groundPhysicBody.isDynamic = false
         ground.physicsBody = groundPhysicBody
+        ground.physicsBody?.categoryBitMask = 0b100
+        ground.physicsBody?.collisionBitMask = 0b001
         addChild(ground)
         
         grounds += 1
@@ -104,11 +126,22 @@ class GravityScene: SKScene {
         }
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("nargas com matt hj?")
+        if contact.bodyA.node?.name == "player" && contact.bodyB.node?.name == "avalanche" {
+            print("morreu")
+        } else if contact.bodyA.node?.name == "avalanche" && contact.bodyB.node?.name == "player" {
+            print("morreu")
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         cameraNode.position.y = player.position.y
+        pointsLabel.position.y = player.position.y + 150
         
         if Int(player.position.y) + (groundHeight * numberOfFutureGrounds) < grounds * groundHeight {
             createGround()
+            updateLabel()
         }
     }
 }
